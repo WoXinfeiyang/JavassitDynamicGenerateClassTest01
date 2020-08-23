ed75758b4d88354ac84da71da64272c4b9e10943
andyfu <815965325@qq.com>
2020-08-23 14:46:51 +0800

    添加工程，通过Javassit动态修改字节码的方式生成类

---
+++ src/JavassitDynamicGenerateClassTest01.java
@@ -0,0 +1,63 @@
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+import javassist.*;
+import javassist.bytecode.AccessFlag;
+import javassist.compiler.CompileError;
+
+public class JavassitDynamicGenerateClassTest01 {
+
+	
+	public static void dynGenerateClass01(){
+		ClassPool pool = ClassPool.getDefault();
+		
+		CtClass ctClass= pool.makeClass("GenerateClass01");
+		
+		try{
+			CtField fieldId=new CtField(CtClass.intType,"id",ctClass);
+			fieldId.setModifiers(AccessFlag.PUBLIC);
+			ctClass.addField(fieldId);
+				
+			CtField fieldName=new CtField(pool.get(String.class.getName()),"name",ctClass);
+			fieldName.setModifiers(AccessFlag.PUBLIC);
+			ctClass.addField(fieldName);
+			
+			CtConstructor ctConstructor= CtNewConstructor.make("public GenerateClass01(int id,String name){this.id=id;this.name=name;}",ctClass);
+			ctClass.addConstructor(ctConstructor);
+			
+			CtMethod methodShowInfo=CtNewMethod.make("public void showInfo(){System.out.println(\"id=\"+id+\",name=\"+name);}", ctClass);
+			ctClass.addMethod(methodShowInfo);
+			ctClass.writeFile(".\\bin");
+			
+			Class cl= Class.forName("GenerateClass01");
+			Object obj= cl.getConstructor(new Class[]{int.class,String.class}).newInstance(new Object[]{Integer.valueOf(101),"zhangsan"});
+			Method method=cl.getMethod("showInfo", null);
+			method.invoke(obj, null);
+			
+		} catch (CannotCompileException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch (NotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		dynGenerateClass01();
+	}
+
+}
